b34c7924ab9b53e728a7a5037fa446e256625185
Sarah Vedersøe <eaaslv@students.eaaa.dk>
2020-04-08 15:27:41 +0200

    Update script.js

--- wp-content/plugins/pop-up-plugin/js/script.js
+++ wp-content/plugins/pop-up-plugin/js/script.js
@@ -1,12 +1,6 @@
-// $(document).ready(function() {
-//    $('#popupbutton').click(function(){
-//        $('#popup').hide();
-//    });
-// }); 
-
-jQuery(document).ready(function( $ ) {
-  $('.#popupbutton').on('click', function() {
-    $('#popup').hide();
-  })
-	
+$(document).ready(function() {
+$('#popupbutton').click(function(){
+$('#popup').hide();
 });
+}); 
+
