0c2729f1f20c0a84af1ca747f44c24cb370a9bcf
Sarah Vedersøe <eaaslv@students.eaaa.dk>
2020-04-09 11:58:38 +0200

    Update index.php

--- wp-content/plugins/pop-up-plugin/index.php
+++ wp-content/plugins/pop-up-plugin/index.php
@@ -3,7 +3,7 @@
 * Plugin Name: Pop-Up Konkurrence
 * Plugin URI: https://sarahvedersoe.dk/storyscape/pop-up-konkurrence
 * Description: Dette er et pop-up vindue plugin. Pluginet er lavet i HTML5, CSS, JS og PHP
-* Version: 0.0.1
+* Version: 0.1.2
 * Author: Sarah Vedersøe    
 * Author: https://sarahvedersoe.dk/storyscape/pop-up-konkurrence
 * License: GPL2
@@ -15,15 +15,15 @@ function popup_form()
 { 
     $content = '';
     $content .= '<section id="popup" style="background-color: white; float: right;>';
-    $content .= '<a href="http://www.google.com">';
-    $content .= '<div id="popupbutton" style="float: right; margin-right: 10px; margin-top:-10px; margin-left: -20px;">X</div>';
+    $content .= '<a href="">';
+    $content .= '<div id="popupbutton" style="float: right; margin-right: 10px; margin-top:-5px; margin-left: -20px;  cursor: pointer;">X</div>';
     $content .= '<div id="koglejagtenh">';
     $content .= '<h1>KOGLEJAGTEN</h1>';
     $content .= '</div>';
     $content .= '<div id="animationsigte">';
     $content .= '<img src=" '.plugins_url("pop-up-plugin/images/sigtekogle.gif").' " alt="kogle i sigtekorn" id="sigtekogle" style="box-shadow: none;">';
     $content .= '</div>';
-    $content .= '<div id="p">Vær med i konkurrencen om at vinde en kasse af dine favorit Kogle øl! Tag vanterne på og bevæg dig ud i skoven. Indsaml de flotteste kogler og pynt hjemmet med dem. Se mere om konkurrencen her! </div>';
+    $content .= '<div id="p">Vær med i konkurrencen om at vinde en kasse af dine favorit Kogle øl! Tag vanterne på og bevæg dig ud i skoven. Indsaml de flotteste kogler og pynt hjemmet med dem. <br> Se mere om konkurrencen under Koglejagten! </div>';
     $content .= '</section>';
      
      return $content;
